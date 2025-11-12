f24a1137358c5046eb8b73182fd5b42dbd994340
郭修传 <guochuan01@163.com>
2025-11-12 16:56:39 +0800

    项目初始化

--- games/starRail/execute/bt_login.py
+++ games/starRail/execute/bt_login.py
@@ -14,9 +14,9 @@ def logins():
     game_start(windows_title)
     for zh in c.zhs:
         登录(zh)
-        # 邮件领取()
+        邮件领取()
         # 巡星之礼领取()
-        兑换码()
+        # 兑换码()
         if c.is_focus():
             c.control.activate()
         log('等待按下0')
@@ -37,5 +37,5 @@ zhs = [
 ]
 
 if __name__ == '__main__':
-    # logins()
-    login_one(6)
+    logins()
+    # login_one(4)
--- games/ys/execute/ys_login.py
+++ games/ys/execute/ys_login.py
@@ -36,4 +36,4 @@ zhs = [
 if __name__ == '__main__':
 
     logins()
-    # login_one(2)
+    # login_one(7)
